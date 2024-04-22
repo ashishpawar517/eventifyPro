699f55358803d8ad8f3e73d87bf120c093c9aad9
Aashish Pawar <ashishpawar517.icloud@gmail.com>
2024-04-22 13:07:59 +1000

    build: initial project setup files added (#2)

---
+++ src/main/java/com/aashishlabs/eventifypro/EventifyProApplication.java
@@ -0,0 +1,13 @@
+package com.aashishlabs.eventifypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyProApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EventifyProApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/aashishlabs/eventifypro/EventifyProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashishlabs.eventifypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyProApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
