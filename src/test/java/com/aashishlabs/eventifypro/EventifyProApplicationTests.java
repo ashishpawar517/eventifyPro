2e7fd30fd0b3fa7b2796457bcec0983eef439be9
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2024-04-22 19:50:59 +1000

    test: added sample test for CI test

--- src/test/java/com/aashishlabs/eventifypro/EventifyProApplicationTests.java
+++ src/test/java/com/aashishlabs/eventifypro/EventifyProApplicationTests.java
@@ -1,5 +1,6 @@
 package com.aashishlabs.eventifypro;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class EventifyProApplicationTests {
 
   @Test
   void contextLoads() {
+    Assertions.assertEquals(7, 7);
   }
 
+  @Test
+  void sampleTest() {
+    Assertions.assertEquals(77, 7);
+  }
 }
