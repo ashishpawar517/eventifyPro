97255ce5f3ac4a24a4e9307122e350f911221620
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2024-04-23 18:22:04 +1000

    update EventUserService.java

--- src/main/java/com/aashishlabs/eventifypro/eventuser/service/EventUserService.java
+++ src/main/java/com/aashishlabs/eventifypro/eventuser/service/EventUserService.java
@@ -1,13 +1,17 @@
 package com.aashishlabs.eventifypro.eventuser.service;
 
+import com.aashishlabs.eventifypro.commons.auth.Role;
 import com.aashishlabs.eventifypro.commons.enums.ErrorCode;
 import com.aashishlabs.eventifypro.eventuser.exception.EventUserException;
 import com.aashishlabs.eventifypro.eventuser.mapper.EventUserDTOMapper;
 import com.aashishlabs.eventifypro.eventuser.model.EventUser;
 import com.aashishlabs.eventifypro.eventuser.model.EventUserDTO;
 import com.aashishlabs.eventifypro.eventuser.repository.IEventUserRepository;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,10 +20,12 @@ class EventUserService implements IEventUserService {
 
   private final IEventUserRepository eventUserRepository;
   private final EventUserDTOMapper eventUserDTOMapper = new EventUserDTOMapper();
+  private final PasswordEncoder passwordEncoder;
 
   @Autowired
-  EventUserService(IEventUserRepository repository) {
+  EventUserService(IEventUserRepository repository, PasswordEncoder passwordEncoder) {
     this.eventUserRepository = repository;
+    this.passwordEncoder = passwordEncoder;
   }
 
   @Override
@@ -33,21 +39,31 @@ class EventUserService implements IEventUserService {
   }
 
   @Override
-  public EventUserDTO createEventUser(String username, String firstName, String lastName,
+  public EventUser createEventUser(String username, String firstName, String lastName,
       String emailAddress, String password) throws EventUserException {
 
     if (eventUserRepository.findEventUserByUsername(username).isPresent()
-        ) {
+    ) {
       throw new EventUserException("Username already exists", ErrorCode.DUPLICATE_USERNAME);
     }
-    if(eventUserRepository.findEventUserByEmailAddress(emailAddress).isPresent()){
+    if (eventUserRepository.findEventUserByEmailAddress(emailAddress).isPresent()) {
       throw new EventUserException("Email already exists", ErrorCode.DUPLICATE_EMAIL);
     }
 
-    EventUser eventUser = new EventUser(username, firstName, lastName, emailAddress, password);
+    EventUser eventUser = new EventUser(username, firstName, lastName, emailAddress,
+        passwordEncoder.encode(password));
+
+    eventUser.setCreatedDate(LocalDateTime.now());
+    eventUser.setUpdatedDate(LocalDateTime.now());
+    eventUser.setRole(Role.USER);
 
     EventUser createdUser = eventUserRepository.save(eventUser);
     log.info("New user created with username : {}", createdUser.getUsername());
-    return eventUserDTOMapper.apply(createdUser);
+    return createdUser;
+  }
+
+  @Override
+  public Optional<EventUser> findByUsername(String username) {
+    return eventUserRepository.findEventUserByUsername(username);
   }
 }
