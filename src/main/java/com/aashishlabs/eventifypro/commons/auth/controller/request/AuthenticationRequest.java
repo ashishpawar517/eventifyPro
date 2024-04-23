5a57ed93669550570cef715c8c9ec3be66b8c142
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2024-04-23 18:21:33 +1000

    update AuthenticationRequest.java

--- src/main/java/com/aashishlabs/eventifypro/commons/auth/controller/request/AuthenticationRequest.java
+++ src/main/java/com/aashishlabs/eventifypro/commons/auth/controller/request/AuthenticationRequest.java
@@ -1,2 +1,16 @@
-package com.aashishlabs.eventifypro.commons.auth.controller.request;public class AuthenticationRequest {
+package com.aashishlabs.eventifypro.commons.auth.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+  String password;
+  private String username;
 }
