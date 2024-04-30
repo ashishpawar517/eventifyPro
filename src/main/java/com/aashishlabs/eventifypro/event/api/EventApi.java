18e45209267082baab3520c5484d3b4ddc69d81f
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2024-04-30 14:36:58 +1000

    update EventApi.java

--- src/main/java/com/aashishlabs/eventifypro/event/api/EventApi.java
+++ src/main/java/com/aashishlabs/eventifypro/event/api/EventApi.java
@@ -1,10 +1,33 @@
 package com.aashishlabs.eventifypro.event.api;
 
+import com.aashishlabs.eventifypro.event.api.request.CreateEventRequest;
+import com.aashishlabs.eventifypro.event.exception.EventException;
+import com.aashishlabs.eventifypro.event.service.IEventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/event")
+@RequiredArgsConstructor
 public class EventApi {
 
+  private final IEventService eventService;
+
+  @PostMapping("/add")
+  @PreAuthorize("hasAuthority('ADMIN')")
+  public ResponseEntity<?> createEvent(@Valid @RequestBody CreateEventRequest request)
+      throws EventException {
+    return new ResponseEntity<>(
+        eventService.createEvent(request.getName(),
+            request.getDescription(),
+            request.getLocation(),
+            request.getDate()), HttpStatus.OK);
+  }
 }
