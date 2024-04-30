5185ba2bead06dcf2956bf66329a5b728ed19070
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2024-04-30 14:36:52 +1000

    update SecurityConfig.java

--- src/main/java/com/aashishlabs/eventifypro/commons/config/SecurityConfig.java
+++ src/main/java/com/aashishlabs/eventifypro/commons/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.aashishlabs.eventifypro.commons.config;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 import com.aashishlabs.eventifypro.commons.auth.filter.JwtAuthFilter;
+import com.aashishlabs.eventifypro.commons.auth.handler.CustomAccessDeniedHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -16,24 +20,28 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
   private final JwtAuthFilter jwtAuthFilter;
   private final AuthenticationProvider authProvider;
+  private final CustomAccessDeniedHandler accessDeniedHandler;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-        .csrf(AbstractHttpConfigurer::disable)
+        .csrf(csrf -> csrf.ignoringRequestMatchers(toH2Console()).disable())
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/api/v1/auth/**").permitAll()
-            .requestMatchers("/api/v1/events/**").hasRole("ADMIN")
+            .requestMatchers(toH2Console()).permitAll()
             .anyRequest().authenticated()
         )
+        .exceptionHandling(exception -> exception.accessDeniedHandler(accessDeniedHandler))
         .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(authProvider)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
         .httpBasic(Customizer.withDefaults())
+        .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
         .build();
   }
 }
