c9dd6ed2af17b34edeab038af996db4581f96105
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2024-04-30 14:37:39 +1000

    update IEventRepository.java

--- src/main/java/com/aashishlabs/eventifypro/event/repository/IEventRepository.java
+++ src/main/java/com/aashishlabs/eventifypro/event/repository/IEventRepository.java
@@ -1,8 +1,19 @@
 package com.aashishlabs.eventifypro.event.repository;
 
 import com.aashishlabs.eventifypro.event.model.Event;
+import java.time.LocalDate;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IEventRepository extends CrudRepository<Event, Long> {
 
+  Optional<Event> findEventByEventId(Long eventId);
+
+  Optional<Event> findEventByDateAndLocation(LocalDate date, String location);
+
+  long deleteByEventId(Long eventId);
+
+  Page<Event> findAll(Pageable pageable);
 }
