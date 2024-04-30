07697e7164fb04d76d1cfb1ba9fa7d0a6af40d90
ashishpawar517 <ashishpawar517.icloud@gmail.com>
2024-04-30 14:37:57 +1000

    update IEventService.java

--- src/main/java/com/aashishlabs/eventifypro/event/service/IEventService.java
+++ src/main/java/com/aashishlabs/eventifypro/event/service/IEventService.java
@@ -1,5 +1,21 @@
 package com.aashishlabs.eventifypro.event.service;
 
+import com.aashishlabs.eventifypro.event.exception.EventException;
+import com.aashishlabs.eventifypro.event.model.EventDTO;
+
 public interface IEventService {
 
+  EventDTO createEvent(String eventName,
+      String eventDescription,
+      String eventLocation,
+      String eventDate) throws EventException;
+
+  EventDTO updateEvent(Long eventId,
+      String eventName,
+      String eventDescription,
+      String eventLocation,
+      String eventDate) throws EventException;
+
+
+  void deleteEvent(Long eventId) throws EventException;
 }
